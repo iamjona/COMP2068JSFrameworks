b248b760c07ab00445e4115ad11963991320f498
iamjona <119086140+iamjona@users.noreply.github.com>
2023-09-19 15:18:07 -0400

    created new application
    created new application

---
+++ consoleApplicatiom/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace consoleApplicatiom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github Desktop!");
+        }
+    }
+}
